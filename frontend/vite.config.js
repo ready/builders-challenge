38d1a7d13b1ff5e16186f58682a28a04390eeb7e
vrabelt <vrabeltyler@gmail.com>
2022-11-10 10:23:28 -0500

    chore(#1): remove portal vite global vars

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { execSync } from 'child_process'
-import { readFileSync } from 'fs'
-
-/**
- * Reads the automated changelog updates file
- * Returns an empty array if the file is malformatted or doesn't exist
- * @returns an array of changelog updates for injecting into a global variable
- */
-function readChangelogFile () {
-  try {
-    return JSON.parse(readFileSync('automatic_changelog.json').toString())
-  } catch {
-    return []
-  }
-}
 
 export default defineConfig({
   plugins: [
@@ -23,11 +8,6 @@ export default defineConfig({
     tsconfigPaths()
   ],
   loader: { '.js': 'jsx' },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    GIT_BRANCH: JSON.stringify(execSync('git rev-parse --abbrev-ref HEAD').toString().trimEnd()),
-    UPDATES_CHANGELOG: readChangelogFile()
-  },
   server: {
     port: 3000
   }
