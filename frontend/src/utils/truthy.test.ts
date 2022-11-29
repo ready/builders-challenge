a3fea55263778bc7fa70cd61511dded02f532949
River <riverliway@gmail.com>
2022-11-29 12:07:35 -0800

    refact: remove `areMatchingArrays` from `truthy`

--- frontend/src/utils/truthy.test.ts
+++ frontend/src/utils/truthy.test.ts
@@ -1,4 +1,4 @@
-import truthy, { areMatchingArrays } from './truthy'
+import truthy from './truthy'
 
 describe('truthy of', () => {
   test('undefined', () => {
@@ -91,62 +91,5 @@ describe('truthy of', () => {
 
   test('string false', () => {
     expect(truthy('false')).toBe(true)
-  })
-})
-
-describe('areMatchingArrays of', () => {
-  test('small matching single-typed arrays', () => {
-    expect(areMatchingArrays(['hello', 'goodbye', 'ciao'], ['hello', 'goodbye', 'ciao'])).toBe(true)
-  })
-
-  test('small matching multi-typed arrays', () => {
-    expect(areMatchingArrays(['hello', 17, false], ['hello', false, 17])).toBe(true)
-  })
-
-  test('small same length unmatching single-typed arrays', () => {
-    expect(areMatchingArrays(['hello', 'goodbye'], ['hello', 'ciao'])).toBe(false)
-  })
-
-  test('small same length unmatching multi-typed arrays', () => {
-    expect(areMatchingArrays(['hello', false], ['hello', true])).toBe(false)
-  })
-
-  test('small different length single-typed arrays', () => {
-    expect(areMatchingArrays(['hello', 'goodbye'], ['hello', 'goodbye', 'ciao'])).toBe(false)
-  })
-
-  test('small different length multi-typed arrays', () => {
-    expect(areMatchingArrays(['hello', true], ['hello', 56.4, true])).toBe(false)
-  })
-
-  const arr01: number[] = [...Array(1001).keys()]
-  const arr02: number[] = [...Array(1001).keys()]
-
-  test('large matched single-typed arrays', () => {
-    expect(areMatchingArrays(arr01, arr02)).toBe(true)
-  })
-
-  test('large unmatched single-typed arrays', () => {
-    arr02[17] = 2
-    expect(areMatchingArrays(arr01, arr02)).toBe(false)
-  })
-
-  test('large matched multi-typed arrays', () => {
-    const arr03: Array<number | boolean> = [...Array(1001).keys()]
-    const arr04: Array<number | boolean> = [...Array(1001).keys()]
-    arr03[17] = true
-    arr04[17] = true
-
-    expect(areMatchingArrays(arr03, arr04)).toBe(true)
-  })
-
-  test('large unmatched multi-typed arrays', () => {
-    const arr03: Array<number | boolean> = [...Array(1001).keys()]
-    const arr04: Array<number | boolean> = [...Array(1001).keys()]
-    arr03[17] = true
-    arr04[17] = true
-    arr03[18] = false
-
-    expect(areMatchingArrays(arr03, arr04)).toBe(false)
   })
 })
--- frontend/src/utils/truthy.ts
+++ frontend/src/utils/truthy.ts
@@ -14,25 +14,4 @@ function truthy (obj: unknown): obj is Record<string | number | symbol, unknown>
   return true
 }
 
-/**
- * Checks if two arrays are equivalent (the contents match regardless of order)
- * Only checks for shallow equivalence
- * @param array1 - The first array to compare the second array to
- * @param array2 - The second array to compare the first array to
- * @returns true iff the two arrays are equivalent
- */
-export function areMatchingArrays (array1: unknown[], array2: unknown[]): boolean {
-  if (array1.length !== array2.length) return false
-
-  // For arrays of less than around 1000 elements, Array#indexOf is the fastest
-  // Shown by the benchmark here: https://stackoverflow.com/questions/31091772/javascript-es6-computational-time-complexity-of-collections
-  if (array1.length < 1000) {
-    return array1.every(value => array2.indexOf(value) !== -1) // eslint-disable-line @typescript-eslint/prefer-includes
-  }
-
-  // For larger datasets, Set#has is faster
-  const set = new Set(array2)
-  return array1.every(value => set.has(value))
-}
-
 export default truthy
