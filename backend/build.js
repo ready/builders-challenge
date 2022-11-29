1adc2d27da15a714b58a706003cb56d47336c249
River <riverliway@gmail.com>
2022-11-29 12:30:32 -0800

    fix: remove mistake in build script

--- backend/build.js
+++ backend/build.js
@@ -3,6 +3,5 @@ require('esbuild').build({
   bundle: true,
   platform: 'node',
   external: [ 'pg-native', 'better-sqlite3', 'mysql2', 'oracledb', 'sqlite3', 'tedious', 'mysql', 'pg-query-stream', 'assert', 'fs', 'os', 'https', 'http', 'stream', 'tty', 'zlib', 'timers', 'path', 'crypto', 'dns', 'module', 'process', 'http2', 'child_process', ],
-  outfile: 'build/index.js',
-  ignore
+  outfile: 'build/index.js'
 }).catch(() => process.exit(1))
