0586a6ad505e4f6c448567fb1c202a70ed37aedd
Antonio Lee <10659631+leeajy@users.noreply.github.com>
2023-01-24 14:19:49 -0600

    clean-up

--- airflow/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-airflow db init
-airflow scheduler & 
-airflow users create --role Admin --username admin --firstname admin --lastname admin --email admin --password builders123 
-
-airflow webserver
